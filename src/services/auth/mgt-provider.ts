d836bf9ba2d2141efa65aae1827bf76f50d5d0c6
sunny-ivan <166166954+sunny-ivan@users.noreply.github.com>
2024-05-07 05:27:49 +0800

    add mgt-provider

---
+++ src/services/auth/mgt-provider.ts
@@ -0,0 +1,35 @@
+import { ProviderState, Providers, SimpleProvider } from "@microsoft/mgt-react";
+import pca from "./configure";
+import { getAccount, login } from "./utils";
+
+async function getAccessToken(scopes: string[]): Promise<string> {
+  if (!!scopes.some((element) => element.startsWith("Files."))) {
+    scopes = scopes.filter((element) => !element.startsWith("Files."));
+    scopes.push("Files.ReadWrite");
+  }
+
+  if (!pca.getAllAccounts) {
+    throw new Error("No such method getAllAccounts");
+  }
+  const account = await getAccount();
+  const result = await pca.acquireTokenSilent({
+    account: account,
+    scopes,
+  });
+  return result.accessToken;
+}
+
+function logout() {
+  return pca.logoutRedirect();
+}
+
+const provider = new SimpleProvider(
+  getAccessToken,
+  () => {
+    Providers.globalProvider.setState(ProviderState.SignedIn);
+    return login();
+  },
+  logout
+);
+
+export default provider;
