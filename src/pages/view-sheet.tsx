137ba1c3a30660746dd3a48a0df3acb504790a0b
sunny-ivan <166166954+sunny-ivan@users.noreply.github.com>
2024-05-07 08:55:19 +0800

    add checking in view-sheet

--- src/pages/view-sheet.tsx
+++ src/pages/view-sheet.tsx
@@ -1,12 +1,17 @@
 import { useMsal } from "@azure/msal-react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { hasPreferDocument } from "../services/storage/prefer-document";
 
 function ViewSheet() {
   const { instance: pca } = useMsal();
   const navigate = useNavigate();
 
   useEffect(() => {
+    if (!hasPreferDocument()) {
+      navigate("/choose");
+    }
+
     pca.initialize().then(() => {
       if (pca.getAllAccounts().length <= 0) {
         navigate("/login");
