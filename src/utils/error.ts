9095a4b137dea8e66bde0bb6553972eb7250d2af
sunny-ivan <166166954+sunny-ivan@users.noreply.github.com>
2024-05-09 01:04:43 +0800

    fix bug that prevented display correct error message

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -5,6 +5,12 @@ export function errorMessage(error: unknown): string {
     return `${error.status} ${error.statusText}`;
   } else if (error instanceof Error) {
     return error.message;
+  } else if (error && Object.prototype.hasOwnProperty.call(error, "message")) {
+    try {
+      return (error as any).message;
+    } catch (error) {
+      return "Unknown error";
+    }
   } else if (typeof error === "string") {
     return error;
   } else {
