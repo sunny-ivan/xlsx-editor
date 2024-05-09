ddebd2d0d19c327251cf5da524e982eec40df7f8
sunny-ivan <166166954+sunny-ivan@users.noreply.github.com>
2024-05-10 06:40:06 +0800

    Merge branch 'main' to fix a mistake

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -3,11 +3,7 @@ import AuthProvider from "./auth-provider";
 import MuiThemeProvider from "./mui-theme-provider";
 import EmotionThemeProvider from "./emotion-theme-provider";
 import { ConfirmProvider } from "material-ui-confirm";
-<<<<<<< HEAD
 import SnackbarProvider from "./SnackbarProvider";
-=======
-import { SnackServiceProvider } from "@insdim-lab/mui-plugins";
->>>>>>> aaa7069e82d743ba2a1beab7200b64f9c5b219de
 
 type SlotProps = {
   children: ReactNode;
@@ -20,15 +16,9 @@ export default function GlobalProvider(props: SlotProps) {
     <MuiThemeProvider>
       <EmotionThemeProvider>
         <ConfirmProvider>
-<<<<<<< HEAD
           <SnackbarProvider>
             <AuthProvider children={children} />
           </SnackbarProvider>
-=======
-          <SnackServiceProvider>
-            <AuthProvider children={children} />
-          </SnackServiceProvider>
->>>>>>> aaa7069e82d743ba2a1beab7200b64f9c5b219de
         </ConfirmProvider>
       </EmotionThemeProvider>
     </MuiThemeProvider>
