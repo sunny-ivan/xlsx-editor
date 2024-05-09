c413bbc8b3278e905ce5099c15d4e5882afe7314
sunny-ivan <166166954+sunny-ivan@users.noreply.github.com>
2024-05-09 11:57:17 +0800

    Temporarily switch development to playground file WARNING: all builds in the production environment will inevitably FAIL indubitably if this commit is inadvertently pushed

--- src/pages/view-table/index.tsx
+++ src/pages/view-table/index.tsx
@@ -2,6 +2,7 @@ import { useMsal } from "@azure/msal-react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { hasPreferDocument } from "../../services/storage/prefer-document";
+import Playground from "./playground-table";
 
 function ViewTable() {
   const { instance: pca } = useMsal();
@@ -19,7 +20,7 @@ function ViewTable() {
     });
   });
 
-  return <div>ViewTable</div>;
+  return <Playground />;
 }
 
 export default ViewTable;
